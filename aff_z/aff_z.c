0fe711b9174023b6a04e1f2f51e005124dc3ef3d
ymarcais <119794979+ymarcais@users.noreply.github.com>
2023-09-02 10:51:15 +0200

    aff_z.c

--- aff_z/aff_z.c
+++ aff_z/aff_z.c
@@ -16,13 +16,13 @@ int main(int ac, char **av)
 {
     char    *str;
 
-    str = av[1];
     if (ac != 2)
     {
         write(1, "a", 1);
         write(1, "\n", 1);
         return (0);
-    }    
+    }
+    str = av[1];
     while(*str)
     {
         if (*str == 'a')
